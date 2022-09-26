41e1c60ec616dcd9a274663061ff637fd5367569
SANG-SANG <66198932+pktony@users.noreply.github.com>
2022-09-26 16:02:36 +0900

    [리펙터링] ILootable 구현
     루팅 관련 반복코드 리펙터

---
+++ Assets/Script/Items/Interactables/Weapon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+    public WeaponData weaponData;
+
+    public override void LootAction()
+    {
+        int sameCount = 0;
+        for (int i = 0; i < player.Inven.slotCount; i++)
+        {// Check if Weapon == in slot weapons
+            if (player.Inven.Slots[i].WeaponSlotData == weaponData)
+            {
+                sameCount++;
+            }
+        }
+
+        if (sameCount < 1)
+        {
+            player.Inven.AddItem(weaponData);
+            this.gameObject.tag = "Player";
+            Destroy(this.gameObject);
+            int index = player.CurrentWeaponIndex;
+            player.CurrentWeaponIndex++;
+            if (index == player.CurrentWeaponIndex)
+            {
+                player.CurrentWeaponIndex--;
+            }
+        }
+        else
+        {
+            Debug.Log("");
+        }
+    }
+}
